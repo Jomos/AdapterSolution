20dd302864371555ea3891804712f1159d7d16f1
Jomos <jorgen.olsson@yh.nackademin.se>
2014-10-13 15:10:08 +0200

    Tests Working

---
+++ Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductLib;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductLib.Product product=new Product(){BarCode = "123456",Title = "Hairdryer",UnitPrice = 12};
+
+
+        }
+    }
+}
